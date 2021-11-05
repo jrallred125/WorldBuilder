94310f11d0fc445e3ae0a34e175d856a63e51d8a
jrallred125 <39539774+jrallred125@users.noreply.github.com>
2021-11-05 14:01:32 -0600

    Worked on the character view and changed the home view.

---
+++ WorldBuilderV2/WorldBuilderWPF/MVVM/Model/Character.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldBuilderWPF.MVVM.Model
+{
+    public class Character
+    {
+        /// <summary>
+        /// The name of the Character.
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// The race that the character is.
+        /// </summary>
+        public string Race { get; set; }
+        /// <summary>
+        /// The age of the character in years.
+        /// </summary>
+        public int Age { get; set; }
+        /// <summary>
+        /// The weight of the character in lbs.
+        /// </summary>
+        public string Height { get; set; }
+        /// <summary>
+        /// How much the character weights.
+        /// </summary>
+        public string Weight { get; set; }
+        /// <summary>
+        /// The gender of the character.
+        /// </summary>
+        public string Gender { get; set; }
+        /// <summary>
+        /// The way the character looks and how they dress. 
+        /// </summary>
+        public string Apparance { get; set; }
+        /// <summary>
+        /// The Character's personality. 
+        /// </summary>
+        public string Personality { get; set; }
+        /// <summary>
+        /// What the character likes. 
+        /// </summary>
+        public string Likes { get; set; }
+        /// <summary>
+        /// What the character dislikes.
+        /// </summary>
+        public string Dislikes { get; set; }
+        /// <summary>
+        /// The story of the character before they were made.
+        /// </summary>
+        public string Backstory { get; set; }
+        /// <summary>
+        /// The Image to be used for the character.
+        /// </summary>
+        public string ProfileImage { get; set; }
+    }
+}
--- WorldBuilderV2/WorldBuilderWPF/MainWindow.xaml.cs
+++ WorldBuilderV2/WorldBuilderWPF/MainWindow.xaml.cs
@@ -25,6 +25,25 @@ namespace WorldBuilderWPF
             InitializeComponent();
         }
 
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void MinmizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
