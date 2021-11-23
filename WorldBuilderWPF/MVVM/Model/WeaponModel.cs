782d616365634be462fd845d658f7608b826d32f
jrallred125 <39539774+jrallred125@users.noreply.github.com>
2021-11-23 15:44:00 -0700

    Updated some of the view and added a new model to be tested.

---
+++ WorldBuilderWPF/MVVM/Model/WeaponModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldBuilderWPF.MVVM.Model
+{
+    [Serializable]
+    public class WeaponModel : ItemModel
+    {
+        public string Damage { get; set; }
+
+        public WeaponModel()
+        {
+            Type = "Weapon";
+        }
+    }
+}
